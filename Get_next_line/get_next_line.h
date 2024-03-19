8e9a57890e235bb17e9c42621830f8e4c1b9e7a4
Bibendam <161604280+Bibendam@users.noreply.github.com>
2024-03-19 16:01:58 +0100

    Add files via upload

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drizzo <drizzo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 13:11:26 by drizzo            #+#    #+#             */
+/*   Updated: 2024/03/19 13:46:23 by drizzo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+size_t	ft_strlen(const char *c);
+char	*get_next_line(int fd);
+
+#endif
